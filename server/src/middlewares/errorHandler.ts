061cb3f515dd7014ee7158029c4461dfb3e2a9d9
vickie <is.vickie18@gmail.com>
2023-11-10 23:33:25 +0800

    custom HttpError

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,9 +1,11 @@
+import HttpError from "../utils/HttpError";
 import logger from "../utils/logger";
 import type { ErrorRequestHandler } from "express";
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next): void => {
+	if (err instanceof HttpError) res.status(err.statusCode).send(err.message);
+	else res.status(500).send(err.message);
 	logger.error(err.message);
-	res.status(500).send(err.message);
 }
 
 export default errorHandler;
